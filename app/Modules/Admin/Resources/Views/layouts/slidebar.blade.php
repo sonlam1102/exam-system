61eebab2605cdb18c5e679cddb8173401fa92073
Lưu Thanh Sơn <sonlam1102@hotmail.com>
2018-04-08 16:03:22 +0700

    [Admin]-add feedback to slide bar

--- app/Modules/Admin/Resources/Views/layouts/slidebar.blade.php
+++ app/Modules/Admin/Resources/Views/layouts/slidebar.blade.php
@@ -7,7 +7,7 @@
 
     <li class="active treeview">
       <a href="#">
-        <i class="fa fa-dashboard"></i> <span>User name</span>
+        <i class="fa fa-dashboard"></i> <span>Users</span>
         <span class="pull-right-container">
           <i class="fa fa-angle-left pull-right"></i>
         </span>
@@ -28,6 +28,18 @@
       <ul class="treeview-menu">
         <li><a href="/admin/contest"><i class="fa fa-circle-o"></i> List contest </a></li>
         <!-- <li><a href="/admin/user/list"><i class="fa fa-circle-o"></i>User account</a></li> -->
+      </ul>
+    </li>
+
+     <li class="active treeview">
+      <a href="#">
+        <i class="fa fa-dashboard"></i> <span>Feedback</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="/admin/feedback"><i class="fa fa-circle-o"></i> User feedback </a></li>
       </ul>
     </li>
 
