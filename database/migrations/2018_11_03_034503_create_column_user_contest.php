e62077b4d565907b262cc523759d993f59fff00e
Thanh Son <son.luu@youthdev.net>
2018-11-03 10:49:36 +0700

    edit migration for contest

---
+++ database/migrations/2018_11_03_034503_create_column_user_contest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColumnUserContest extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contest', function (Blueprint $table) {
+            if (!Schema::hasColumn('contest', 'user_id')) {
+                $table->unsignedInteger('user_id');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
