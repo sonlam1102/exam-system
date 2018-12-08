5eac0281d19d17d007124a42184fa288eb3aef3e
Thanh Son <son.luu@youthdev.net>
2018-12-08 09:51:55 +0700

    update admin routes

--- app/Modules/Admin/Routes/web.php
+++ app/Modules/Admin/Routes/web.php
@@ -7,25 +7,33 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'type']], function (
     Route::post('/update/{id}', 'AdminController@update')->where('id', '[0-9]+');
 
     //User info
-    Route::get('/user/list', 'UserController@index');   //List users
-    Route::get('/user/info/{id}', 'UserController@info')->where('id', '[0-9]+');    //User info
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/list', 'UserController@index');   //List users
+        Route::get('/info/{id}', 'UserController@info')->where('id', '[0-9]+');    //User info
+    });
 
     //Contest Info
-    Route::get('/contest', 'ContestController@index');
-    Route::get('/contest/add', 'ContestController@add');
-    Route::post('/contest/add', 'ContestController@add');
-    Route::get('/contest/edit/{id}', 'ContestController@edit')->where('id', '[0-9]+');  //add question request
-    Route::post('/contest/edit/{id}/info', 'ContestController@edit_info')->where('id', '[0-9]+');  //edit contest infp
-    Route::post('/contest/edit/{id}/question', 'ContestController@add_question')->where('id', '[0-9]+');  //add question request
-    Route::delete('/contest/delete/{id}', 'ContestController@deleteContest')->where('id', '[0-9]+');  //delete contest by id
-    Route::delete('/contest/delete/{id}/question', 'ContestController@deleteQuestion')->where('id', '[0-9]+');  //delete question by id
-    Route::get('/contest/{id}/candidate', 'ContestController@listCandidate')->where('id', '[0-9]+');  //get candidtes of contest
+    Route::group(['prefix' => 'contest'], function () {
+        Route::get('/', 'ContestController@index');
+        Route::get('/add', 'ContestController@add');
+        Route::post('/add', 'ContestController@add');
+        Route::get('/edit/{id}', 'ContestController@edit')->where('id', '[0-9]+');  //add question request
+        Route::post('/edit/{id}/info', 'ContestController@edit_info')->where('id', '[0-9]+');  //edit contest infp
+        Route::post('/edit/{id}/question', 'ContestController@add_question')->where('id', '[0-9]+');  //add question request
+        Route::delete('/delete/{id}', 'ContestController@deleteContest')->where('id', '[0-9]+');  //delete contest by id
+        Route::delete('/delete/{id}/question', 'ContestController@deleteQuestion')->where('id', '[0-9]+');  //delete question by id
+        Route::get('/{id}/candidate', 'ContestController@listCandidate')->where('id', '[0-9]+');  //get candidtes of contest
+    });
 
     //Feedback
-    Route::get('/feedback', 'FeedBackController@index');
+    Route::group(['prefix' => 'feedback'], function () {
+        Route::get('/', 'FeedBackController@index');
+    });
 
     //Subject
-    Route::get('/subject', 'SubjectController@index');
-    Route::get('/subject/add', 'SubjectController@add');
-    Route::post('/subject/add', 'SubjectController@add');
+    Route::group(['prefix' => 'subject'], function () {
+        Route::get('/', 'SubjectController@index');
+        Route::get('/add', 'SubjectController@add');
+        Route::post('/add', 'SubjectController@add');
+    });
 });
